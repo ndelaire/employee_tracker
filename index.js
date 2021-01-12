ca3263f4ac89757ace4d3b60a45361c8bab156f3
Nadine Delaire <ndelaire16@gmail.com>
2021-01-11 18:22:24 -0800

    adding to index

--- index.js
+++ index.js
@@ -10,5 +10,63 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    selectOption();
   });
+
+  function selectOption() {
+    inquirer
+      .prompt({
+        name: "selectOption",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add Employee",
+            "View All Employees",
+            "Add Role",
+            "View All Roles",
+            "Add Department",
+            "View All Departments",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.selectOption) {
+        case "Add Employee":
+          addEmployee();
+          break;
+  
+        case  "View All Employees":
+          viewEmployees();
+          break;
+  
+        case  "Add Role":
+          addRole();
+          break;
+  
+        case "View All Roles":
+          viewRoles();
+          break;
+  
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "View All Departments":
+            viewDepartments();
+            break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Update Employee Manager":
+            updateManager();
+            break;
+        
+        case "Exit":
+            connection.end();
+        }
+      });
+  }
